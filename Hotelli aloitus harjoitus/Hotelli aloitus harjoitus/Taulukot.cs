84f6ea546087bd87d3abf912ed909e3a12f0f146
unknown <samu.hamalainen@edu.keuda.fi>
2021-11-22 11:25:37 +0200

    Töitä lisää 22.11.2021

---
+++ Hotelli aloitus harjoitus/Hotelli aloitus harjoitus/Taulukot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Diagnostics;
+
+namespace Hotelli_aloitus_harjoitus
+{
+    class Taulukot
+    {
+        
+
+
+   }
+}
