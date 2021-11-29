8cfdf977c41f59b52d57057ecb383b2d513e1918
Samppa245 <Samu.hamalainen@edu.keuda.fi>
2021-11-29 19:28:02 +0200

    Loppuviimeistelyä

---
+++ Kirjastotyö/ConsoleApp1/Koodi.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Koodi
+    {
+        string Nim;
+        string kirj;
+        string siv;
+        private string onklain;
+        string t;
+        public Koodi(string nimi, string kirjoittaja, string sivut, string onkolainattu, string tunnus)
+        {
+            Nim = "Kirja: " + nimi;
+            kirj = "Kirjoittajan nimi: " + kirjoittaja;
+            siv = "Sivujen määrä: " + sivut;
+            onklain = onkolainattu;
+            t = "Tuoteavain: " + tunnus;
+
+        }
+
+        public string onkokyllä
+
+        {
+           
+            get { return onklain; }
+            set { onklain = value; }
+           
+        }
+
+        
+
+
+        public void lainauscheck()
+        {
+            if (onklain == "Ei")
+            {
+                Console.WriteLine("Kirja on lainattu");
+                
+
+                onkokyllä = "Kyllä";
+                
+            }
+            else
+            {
+                Console.WriteLine("Kirjaa ei voi lainata uuestaan");
+           
+            }
+        }
+
+        public string kirjaint()
+        {
+            if (onklain == "Ei")
+            {
+                return t;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public string kirjannimit()
+        {
+
+            if (onklain == "Ei")
+            {
+                return Nim;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public string Palautus()
+        {
+            if (onklain == "Kyllä")
+            {
+
+                Console.WriteLine("Kirja on palautettu");
+                return onklain = "Ei";
+            }
+            else
+            {
+                Console.WriteLine("Et ole lainannut kirjaa");
+                return "";
+            }
+        }
+        public string kirjoittajat()
+        {
+            if (onklain == "Ei")
+            {
+                return kirj;
+            }
+            else
+            {
+                return "";
+            }
+        }
+            public string sivutt()
+        {
+            if (onklain == "Ei")
+            {
+            
+                return siv;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
